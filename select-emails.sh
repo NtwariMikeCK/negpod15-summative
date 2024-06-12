503585ef389973d644eb6eb2527c53ca940831b8
kelvintawe12 <k.tawe@alustudent.com>
2024-06-12 15:40:28 -0700

    fifth edit to the file

--- select-emails.sh
+++ select-emails.sh
@@ -3,7 +3,7 @@
 # Extract student emails from students-list_0524.txt
 awk -F',' '{print $1}' students-list_0524.txt > student-emails.txt
 
-# Read emails from temp file and display them
+# Read emails from temp file and display them in the temp-emails.txt
 cat temp-emails.txt
 # OR save them to student-emails.txt:
 # cat temp-emails.txt > student-emails.txt
