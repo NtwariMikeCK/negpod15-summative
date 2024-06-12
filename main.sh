f2531c84bb24ba7371a11c82a76c6ace6fb48df3
kelvintawe12 <k.tawe@alustudent.com>
2024-06-12 16:14:50 -0700

    adding the spaces

--- main.sh
+++ main.sh
@@ -175,6 +175,7 @@ while true; do
         echo "----------------------------------------"
 	echo "----------------------------------------"
         echo "Main Menu:"
+	echo
     echo -e "\033[1;34m1. Create Student Record\033[0m"
     echo -e "\033[1;34m2. View All Students\033[0m"
     echo -e "\033[1;34m3. Delete Student\033[0m"
