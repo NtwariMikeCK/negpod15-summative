0ded7a72df14f3a6eb9aca93a7e3e9ec3a0d2b32
root <s.noella@alustudent.com>
2024-06-12 06:08:04 -0700

    view students

--- main.sh
+++ main.sh
@@ -1,1 +1,22 @@
 #!/usr/bin/env bash
+
+
+
+
+
+
+
+
+
+
+
+
+# Function to view all students
+view_stdents() {
+    if [[ -f $student_file ]]; then
+        echo "List of all students:"
+        cat $student_file
+    else
+        echo "No student records found."
+    fi
+}
